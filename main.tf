85a06a8c3a844ed14788e907271a7c11ebf53924
Sertaç Özercan <sozercan@gmail.com>
2017-07-26 16:02:03 -0700

    added azurerm provider

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "azurerm" {
+  subscription_id = "${var.azure_subscription_id}"
+  client_id       = "${var.azure_client_id}"
+  client_secret   = "${var.azure_client_secret}"
+  tenant_id       = "${var.azure_tenant_id}"
+}
+
 resource "azurerm_resource_group" "osrg" {
   name     = "${var.openshift_azure_resource_group}"
   location = "${var.openshift_azure_region}"
