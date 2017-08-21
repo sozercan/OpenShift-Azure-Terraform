29eed224e2a268b6c3173e5d15909473ba45f178
Sertaç Özercan <sozercan@gmail.com>
2017-08-21 00:30:40 -0700

    workaround for nodejs package installation
    https://bugs.centos.org/view.php?id=13669&nbn=1

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -72,6 +72,8 @@ EOF
 
 echo $(date) " - Installing Azure CLI"
 
+rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm
+
 yum -y --enablerepo=epel install nodejs
 
 npm install -g azure-cli
