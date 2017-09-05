d5b64579c37c72ed3a2e27332d5905202ed44043
Sertaç Özercan <sozercan@gmail.com>
2017-09-04 22:44:43 -0700

    add backend address pool for infra http router

--- infra.tf
+++ infra.tf
@@ -54,6 +54,7 @@ resource "azurerm_lb_rule" "osinfralbrule80" {
   backend_port                   = 80
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = "${azurerm_lb_probe.osinfralbprobe80.id}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.osinfralbbepool.id}"
 }
 
 resource "azurerm_lb_probe" "osinfralbprobe80" {
